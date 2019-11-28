0fe765f230c6bd844fb244ff5e248e244e360bef
Otniel <Otnielush@users.noreply.github.com>
2019-11-28 03:41:06 +0200

    update
    added strategies
    changed weights
    fixed problems in analyse

--- 4 in a Row/mind.py
+++ 4 in a Row/mind.py
@@ -1,4 +1,3 @@
-
 # ход - анализ каждого варианта хода на предмет результата и анализ действия которое выбрал опонент
 #
 # анализаторы:
@@ -19,23 +18,24 @@ from time import sleep
 
 wins = [0, 0]
 currPlayer = 0
-board = [[" "]*7 for i in range(6)]  # [rows][columns]
+board = [[" "] * 7 for i in range(6)]  # [rows][columns]
 try:
     file = open("weights.ai", "r")
     loaded = file.readlines()
     file.close()
 except:
     print("File not found")
-    loaded = ["200", "12", "70", "200", "12", "100"]
+    loaded = ["400", "100", "35", "3", "400", "100", "35", "3"]
 
 # print("{}".format(loaded))
 
 # print("len1(rows): %d, len2(cols): %d" % (len(board),len(board[0])))
 print("I wish you a good game")
 
-# board[1][2] = "+"
+
+# board[1][2] = "#"
 # board[5][0] = " "
-# board[5][1] = "+"
+# board[5][1] = "#"
 # board[5][3] = "O"
 # board[4][2] = "O"
 
@@ -53,16 +53,19 @@ def posMoves():
     return posList
 
 # both for the move and for checking
-def maxInRow(player,row,col):
+def maxInRow(player, row, col):
     inRow4 = 0
-    lastTurn = 0
+    inRow3 = 0
+    inRow2 = 0
+    inRowFree = 0
+    numInRow = 0
     inRow = 0
     inRowPosib = 0
     inRowMax = 0
-    sim = "O" if player == 0 else "+"
+    sim = "O" if player == 0 else "#"
     rowTmp = board[row].copy()
     rowTmp[col] = sim
-    start = col-3
+    start = col - 3
     if start < 0: start = 0
     end = col + 4
     if end > 7: end = 7
@@ -70,39 +73,52 @@ def maxInRow(player,row,col):
     for i in range(start, end):
         if rowTmp[i] == " ":
             inRowPosib += 1
+            inRowFree += 1
             inRow = 0
             continue
         if rowTmp[i] == sim:
             inRow += 1
             inRowPosib += 1
+            numInRow += 1
             if inRow > inRowMax: inRowMax = inRow
         else:
+            inRowFree = 0
             inRow = 0
             inRowPosib = 0
+            numInRow = 0
 
-    if inRowMax >= 4: inRow4 = 1
-    if inRowPosib >= 4:
-        if inRow >= 1 and inRowMax >= 2 or inRowMax == 3: lastTurn = 1
-    if len(rowTmp) < 4: inRowPosib = 0
-    return inRow4, inRowPosib, lastTurn*1.5
+    # if len(rowTmp) < 4:
+    #     inRowPosib = 0
+    #     numInRow = 0
+    else:
+        if inRowMax >= 4: inRow4 = 1
+        if inRowPosib >= 4:
+            if inRowMax == 3 or numInRow >= 3 and inRowMax == 2 : inRow3 = 1
+            if inRowMax == 2 or numInRow > 1: inRow2 = 1
+        # if inRowPosib >= 5:
+            # if numInRow >= 2: inRow2 += 1
+    return inRow4, inRow3, inRow2, inRowFree
 
 # both for the move and for checking
 def maxInCol(player, row, col):
     inCol4 = 0
-    lastTurn = 0
+    inCol3 = 0
+    inCol2 = 0
+    numInCol = 0
     inCol = 0
     inColPosib = 0
     inColMax = 0
     colTmp = []
-    sim = "O" if player == 0 else "+"
+    sim = "O" if player == 0 else "#"
     for rows in board:
         colTmp.append(rows[col])
     colTmp[row] = sim
 
     start = row + 4
     if start > 5: start = 5
-    end = row - 3
-    if end < 0: end = 0
+    # end = row - 3
+    # if end < 0: end = 0
+    end = -1
 
     for i in range(start, end, -1):
         if colTmp[i] == " ":
@@ -112,32 +128,39 @@ def maxInCol(player, row, col):
         if colTmp[i] == sim:
             inCol += 1
             inColPosib += 1
+            numInCol += 1
             if inCol > inColMax: inColMax = inCol
         else:
             inCol = 0
             inColPosib = 0
 
-    if inColMax >= 4: inCol4 = 1
-    if inColPosib >= 4:
-        if inColMax == 3 or inCol >= 1 and inColMax >= 2: lastTurn = 1
-    if len(colTmp) < 4: inColPosib = 0
-    return inCol4, inColPosib, lastTurn
+    # if len(colTmp) < 4:
+    #     inColPosib = 0
+    #     numInCol = 0
+    else:
+        if inColMax >= 4: inCol4 = 1
+        if inColPosib >= 4:
+            if inColMax == 3: inCol3 = 1
+            if inColMax == 2: inCol2 = 1
+    return inCol4, inCol3, inCol2, inColPosib - numInCol
 
 # \
 def maxInDiagLeft(player, row, col):
     inDiag4 = 0
-    lastTurn = 0
+    inDiag3 = 0
+    inDiag2 = 0
+    numInDiag = 0
     inDiag = 0
     inDiagPosib = 0
     inDiagMax = 0
     diagTmp = []
-    sim = "O" if player == 0 else "+"
+    sim = "O" if player == 0 else "#"
     for i in range(6):
-        cols = col-(row-i)
+        cols = col - (row - i)
         if cols < 0: continue
         if cols > 6: break
         diagTmp.append(board[i][cols])
-        if row == i: diagTmp[len(diagTmp)-1] = sim
+        if row == i: diagTmp[len(diagTmp) - 1] = sim
 
     diagTmp.reverse()
     for cell in diagTmp:
@@ -148,32 +171,41 @@ def maxInDiagLeft(player, row, col):
         if cell == sim:
             inDiag += 1
             inDiagPosib += 1
+            numInDiag += 1
             if inDiag > inDiagMax: inDiagMax = inDiag
         else:
             inDiag = 0
             inDiagPosib = 0
 
-    if inDiagMax >= 4: inDiag4 = 1
-    if inDiagPosib >= 4:
-        if inDiag >= 1 and inDiagMax >= 2 or inDiagMax == 3: lastTurn = 1
-    if len(diagTmp) < 4: inDiagPosib = 0
-    return inDiag4, inDiagPosib, lastTurn
+    # if len(diagTmp) < 4:
+    #     inDiagPosib = 0
+    #     numInDiag = 0
+    else:
+        if inDiagMax >= 4: inDiag4 = 1
+        if inDiagPosib >= 4:
+            if inDiagMax == 3 or inDiag >= 1 and inDiagMax >= 2: inDiag3 = 1
+            if inDiagMax == 2 or inDiag == 1 and numInDiag > 1: inDiag2 = 1
+        if inDiagPosib >= 5:
+            if inDiag == 1 and numInDiag >= 2: inDiag2 += 1
+    return inDiag4, inDiag3, inDiag2, inDiagPosib - numInDiag
 
 # /
 def maxInDiagRight(player, row, col):
     inDiag4 = 0
-    lastTurn = 0
+    inDiag3 = 0
+    inDiag2 = 0
+    numInDiag = 0
     inDiag = 0
     inDiagPosib = 0
     inDiagMax = 0
     diagTmp = []
-    sim = "O" if player == 0 else "+"
+    sim = "O" if player == 0 else "#"
     for i in range(6):
-        cols = col+(row-i)
+        cols = col + (row - i)
         if cols < 0: break
         if cols > 6: continue
         diagTmp.append(board[i][cols])
-        if row == i: diagTmp[len(diagTmp)-1] = sim
+        if row == i: diagTmp[len(diagTmp) - 1] = sim
 
     diagTmp.reverse()
     for cell in diagTmp:
@@ -184,64 +216,62 @@ def maxInDiagRight(player, row, col):
         if cell == sim:
             inDiag += 1
             inDiagPosib += 1
+            numInDiag += 1
             if inDiag > inDiagMax: inDiagMax = inDiag
         else:
             inDiag = 0
             inDiagPosib = 0
 
-    if inDiagMax >= 4: inDiag4 = 1
-    if inDiagPosib >= 4:
-        if inDiag >= 1 and inDiagMax >= 2 or inDiagMax == 3: lastTurn = 1
-    if len(diagTmp) < 4: inDiagPosib = 0
-    return inDiag4, inDiagPosib, lastTurn
+    # if len(diagTmp) < 4:
+    #     inDiagPosib = 0
+    #     numInDiag = 0
+    else:
+        if inDiagMax >= 4: inDiag4 = 1
+        if inDiagPosib >= 4:
+            if inDiagMax == 3 or inDiag >= 1 and inDiagMax >= 2: inDiag3 = 1
+            if inDiagMax == 2 or inDiag == 1 and numInDiag > 1: inDiag2 = 1
+        if inDiagPosib >= 5:
+            if inDiag == 1 and numInDiag >= 2: inDiag2 += 1
+    return inDiag4, inDiag3, inDiag2, inDiagPosib - numInDiag
 
 # 4 functions for analise in 1 massive
-Review = [maxInRow,maxInCol,maxInDiagLeft,maxInDiagRight]
+Review = [maxInRow, maxInCol, maxInDiagLeft, maxInDiagRight]
+winType = ["row", "column", "left diagonal", "right diagonal"]
 
 # Checking after move
-def win(player,row,col):
+def win(player, row, col):
     # print("Win check")
     winner = "Homo sapiens" if player == 0 else "Artificial Intelligence"
-    four,posToWin,lastTurn = maxInRow(player,row,col)
-    if four == 1:
-       print("Winner by row! -",winner)
-       return 1
-    four, posToWin, lastTurn = maxInCol(player, row, col)
-    if four == 1:
-        print("Winner by column! -",winner)
-        return 1
-    four, posToWin, lastTurn = maxInDiagLeft(player, row, col)
-    if four == 1:
-        print("Winner by left diagonal! -", winner)
-        return 1
-    four, posToWin, lastTurn = maxInDiagRight(player, row, col)
-    if four == 1:
-        print("Winner by right diagonal! -", winner)
-        return 1
+    for i in range(len(Review)):
+        result = Review[i](player, row, col)
+        if result[0] >= 1:
+            print("Winner by {} - {}".format(winType[i], winner))
+            return 1
 
 # for AI
 def makeValuation(player, row, col):
-    winM, goodM, almWin = 0, 0, 0
+    four, three, two, possib = 0, 0, 0, 0
     for i in range(len(Review)):
-        w, g, a = Review[i](player, row, col)
-        winM += w
-        goodM += g
-        almWin += a
-    return winM, goodM, almWin
+        n4, n3, n2, p = Review[i](player, row, col)
+        four += n4
+        three += n3
+        two += n2
+        possib += p
+    # print("{} four {}, thr {}, two {}, po {}-{}".format(col+1, four, three, two, possib,player))
+    return four, three, two, possib
 
 # to show table of game
 def printBoard():
     for i in range(len(board)):
         sleep(0.02)
-        # print("%d " % i, end="")
+        print("%d " % i, end="")
         print("|", end="")
         for k in board[i]:
-            print(" %s |" % k, end='')
+            print("_%s_|" % k, end='')
         print()
-        print("-----------------------------")
-    # print("  ",end="")
+    print("  ",end="")
     for j in range(7):
-        print("  %d " % (j+1), end="")
+        print("  %d " % (j + 1), end="")
     print("\n")
 
 def newGame():
@@ -249,14 +279,14 @@ def newGame():
     board = [[" "] * 7 for i in range(6)]
 
 # Put simon on table
-def move(player,col):
+def move(player, col):
     global board, currPlayer
     if player == 0:
         sim = "O"
-        print("Player move, 'O'")
+        print('Player move, "O"')
     else:
-        sim = "+"
-        print("Ai move, '+'")
+        sim = "#"
+        print('Ai move, "#"')
     r = 5
 
     if not board[0][col] == " ":
@@ -273,83 +303,106 @@ def move(player,col):
     currPlayer %= 2
     return 1
 
-# Strategies with weights
-# 1st - Multiplier aganst Human after agression, 2nd - how many turns
-# Tit-for-tat – מכה מול מכה
-Tit4Tat = [2,1]
-# Grim-Trigger – פוגע חזרה ולא סולח
-GrimTrigger = [2,200]
-# Forgiving Trigger – פוגע חזרה וסולח
-Forgiving = [2,3]
-
-
 def AImove(myNum):
     global agr, agrTurns
-    moveList = posMoves()
-    movePoints = []
-    for i in range(len(moveList)):
-        floor = 1
-        if moveList[i] == -1:
-            movePoints.append(-1)
-            continue
-        winM, goodM, almWin = makeValuation(myNum, moveList[i], i) # AI moves profit
-        winMH, goodMH, almWinH = makeValuation((myNum+1) % 2, moveList[i], i) # Human/opponent moves profit
-        k = i-1 if i > 0 else 0
-        j = i+1 if i < 6 else 6
-        floor -= (moveList[k]+moveList[j]-moveList[i]*2)/10
-        if floor > 1: floor = 1
-        # print(floor)
-        movePoints.append((winM+winMH*agr)*weights[0]+(goodM+goodMH*agr)*weights[1]*floor+(almWin+almWinH*agr)*weights[2])
-    if agr > 1: agrTurns -= 1
+    enemy = (myNum + 1) % 2
+
+    moveList = [0]*7
+    for i in range(7):
+        moveList[i] = movePoints[myNum][i] + movePoints[enemy][i]*agr
+
+    if agr > 0.8: agrTurns -= 1
     if agrTurns <= 0:
-        agr = 1
+        print("Agr stoped")
+        agr = 0.8
         agrTurns = strategy[1]
     # print("{}".format(moveList))
-    # print('{}'.format(movePoints))
-    return movePoints.index(max(movePoints))
+    return moveList.index(max(moveList))
 
 # Cycle game
 def playGame():
-    global wins
+    global wins, movePoints, agr, agrTurns
+    global choiceCl, currPlayer
     while (True):
-        global choiceCl, currPlayer
+        # weights for turn
+        moveList = posMoves()
+        for i in range(len(moveList)):
+            floor = 1
+            if moveList[i] == -1:
+                movePoints[0][i] = -1
+                movePoints[1][i] = -1
+                continue
+            n4H, n3H, n2H, pH = makeValuation(0, moveList[i], i)  # Human/opponent moves profit
+            n4, n3, n2, p = makeValuation(1, moveList[i], i)  # AI moves profit
+            floor -= (sum(moveList) / len(moveList) - moveList[i]) * 0.15
+            if floor > 1: floor = 1
+            movePoints[0][i] = (n4H * weights[4] + n3H * weights[5] + n2H * weights[6] + pH * weights[7])*floor
+            movePoints[1][i] = (n4 * weights[0] + n3 * weights[1] + n2 * weights[2] + p * weights[3])*floor
+            # print("floor",floor)
+        # print("{}".format(movePoints))
+
+        # Human
         if currPlayer == 0:
             try:
-                choiceCl = int(input("Your turn(1-7): "))-1
+                choiceCl = int(input("Your turn(1-7): ")) - 1
                 if 2 == move(currPlayer, choiceCl):
-                # if 2 == move(currPlayer, AImove(currPlayer)):
-                    if currPlayer == 0: wins[0] +=1
-                    else: wins[1] += 1
-                    currPlayer = (currPlayer+1) % 2
-                    break #WINNING()
+                    # if 2 == move(currPlayer, AImove(currPlayer)):
+                    if currPlayer == 0:
+                        wins[0] += 1
+                    else:
+                        wins[1] += 1
+                    currPlayer = (currPlayer + 1) % 2
+                    break  # WINNING()
+
+                # Agressive mode
+                if movePoints[0][choiceCl]+5 < movePoints[1][choiceCl]:
+                    agr = 2
+                    agrTurns = strategy[1]
+                    print("Agr!", movePoints[0][choiceCl], movePoints[1][choiceCl])
             except:
                 print("Not allowed")
+
+        # AI
         elif currPlayer == 1:
             if 2 == move(currPlayer, AImove(currPlayer)):
-                if currPlayer == 0: wins[0] += 1
-                else: wins[1] += 1
+                if currPlayer == 0:
+                    wins[0] += 1
+                else:
+                    wins[1] += 1
                 currPlayer = (currPlayer + 1) % 2
-                break #WINNING()
+                break  # WINNING()
+            else:
+                continue
 
 
+# Strategies with weights
+# 1st - Multiplier aganst Human after agression, 2nd - how many turns
+# Tit-for-tat – מכה מול מכה
+Tit4Tat = [2, 1]
+# Grim-Trigger – פוגע חזרה ולא סולח
+GrimTrigger = [2, 200]
+# Forgiving Trigger – פוגע חזרה וסולח
+Forgiving = [2, 3]
 
 # Some options
-strategy = Tit4Tat
-agr = strategy[0]
+strategy = Forgiving
+agr = 0.8
 agrTurns = strategy[1]
+# Variables
+choiceCl = 0
 weights = [int(x) for x in loaded]
-# print("{}".format(weights))
+movePoints = [[0]*7 for i in range(2)]
+# print("{}".format(movePoints))
 while (True):
     printBoard()
     playGame()
-    print("Score of epic competition:\n Human: %d  AI: %d" % (wins[0],wins[1]))
+    print("Score of epic competition:\n Human: %d  AI: %d" % (wins[0], wins[1]))
     file = open("weights.ai", "w")
 
-    file.writelines(str(weights[i])+"\n" for i in range(3))
+    file.writelines(str(weights[i]) + "\n" for i in range(len(weights)))
 
     file.close()
     ask = input("Play more?(y) or ("")")
     if len(ask) == 0: ask = "y"
     if not ask == "y": break
     newGame()
-
